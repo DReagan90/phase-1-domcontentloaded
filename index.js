a6bef1304a988f4c0005ed8ba65357178110bd0b
ReaganBaimet0 <reagan.baimet@student.moringaschool.com>
2025-03-23 23:17:13 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+    const paragraph = document.querySelector('p');
+    if (paragraph) {
+        paragraph.textContent = 'This is really cool!';
+    }
+});
+
+        
+      
